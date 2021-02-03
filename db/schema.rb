9ce07007f346cc6e42da10c03efee41252e83db7
anthony mcmullen <amcmullen9291@gmail.com>
2021-02-03 00:01:46 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210202233753) do
+ActiveRecord::Schema.define(version: 20210202235632) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.string   "genre"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +32,7 @@ ActiveRecord::Schema.define(version: 20210202233753) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.boolean  "post_status"
   end
 
 end
